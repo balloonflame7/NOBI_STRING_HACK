cfaa91e1d9806eb029e7d5e9ef3e1549c9290fd3
Jatin Sharma <195620992+balloonflame7@users.noreply.github.com>
2025-10-16 20:38:48 +0530

    telegram bot Made by Nobita

---
+++ config.py
@@ -0,0 +1,12 @@
+
+from pyrogram import filters
+import os
+
+class Config:
+    API_ID = "23812672"
+    API_HASH = "297d47a5510f124e7f58387c0682e7ef"
+    TOKEN = "8359190940:AAG8GJZzpDMhls3mjkzaYH3XLpWrUP3pn_k"
+    TOKEN = os.environ.get("TOKEN", None)
+    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    START_PIC = "https://files.catbox.moe/pvj6h7.mp4"
+    SUDOERS = filters.user(["8143754205"])
