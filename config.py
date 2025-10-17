58b84c341dee5ae79c96ac636bf00caf4e4bd00b
balloonflame7 <balloonflame7india@outlook.com>
2025-10-17 09:25:36 +0530

    Config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     API_HASH = "297d47a5510f124e7f58387c0682e7ef"
     TOKEN = "8359190940:AAG8GJZzpDMhls3mjkzaYH3XLpWrUP3pn_k"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_URL = "mongodb+srv://balloonflame7:jatin@#0212@cluster0.7hal6uy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://files.catbox.moe/pvj6h7.mp4"
     SUDOERS = filters.user(["8143754205"])
